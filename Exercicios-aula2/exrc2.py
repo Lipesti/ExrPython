0f90c532406ec3aea1affae7ac4de924a3788a39
Felipe Santos <lipes.santos02@gmail.com>
2022-03-14 09:16:42 -0300

    Update exrc2.py
    Correção.

--- Exercicios-aula2/exrc2.py
+++ Exercicios-aula2/exrc2.py
@@ -2,6 +2,6 @@
 #Escreva um programa em Python que solicite ao usuárioo salário atual e mostre o salário acrescido de 5% de comissão.
 
 salario = float(input('Por gentileza informe seu salario atual R$:'))
-comissao = salario + (salario * 5 / 100)
+comissao =  (salario * 5 / 100)
 
 print('O salario atuaizado com a comissão de 5% é de R$:', salario + comissao)
